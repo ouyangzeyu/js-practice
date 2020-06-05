a8796872e14e1d66c1ca61de85c3cd5497ccf3b5
ouyangzeyu <243175186@qq.com>
2020-06-05 17:08:09 +0800

    更新

--- bind.js
+++ bind.js
@@ -1,8 +1,9 @@
-// 实现一个bind方法
-
+// 实现一个bind方法，核心就是bind会返回一个匿名函数，匿名函数中会执行call或者apply
 Function.prototype.bind = function (context = window, ...arguments) {
-
+    // bind中的this是要操作的函数，在这里先保存一下
+    let _this = this
+    let params = arguments
     return function () {
-        
+        _this.apply(context, params)
     }
 }
---
+++ 防抖与节流.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @param {function} fn 具体的执行函数
+ * @param {number} delay 需要延迟的毫秒数
+ */
+function debounce(fn, delay) {
+
+}
